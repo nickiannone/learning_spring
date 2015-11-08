7ff01f506270fe53080624d47718d933c0769f03
nickiannone <nickiannone@gmail.com>
2015-11-07 19:47:59 -0600

    Completed step 4.1. Connected business logic with controller.

--- src/learningspring/web/InventoryController.java
+++ src/learningspring/web/InventoryController.java
@@ -2,6 +2,8 @@ package learningspring.web;
 
 import java.io.IOException;
 
+import learningspring.service.ProductManager;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,10 +14,14 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-public class HelloController implements Controller {
+public class InventoryController implements Controller {
 
 	protected final Log logger = LogFactory.getLog(getClass());
+	
+	private ProductManager productManager;
 	
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
@@ -23,6 +29,15 @@ public class HelloController implements Controller {
 		String now = (new Date()).toString();
 		
 		logger.info("Returning hello view with " + now);
-		return new ModelAndView("hello", "now", now);
+		
+		Map<String, Object> myModel = new HashMap<String, Object>();
+		myModel.put("now", now);
+		myModel.put("products", this.productManager.getProducts());
+		
+		return new ModelAndView("hello", "model", myModel);
+	}
+	
+	public void setProductManager(ProductManager productManager) {
+		this.productManager = productManager;
 	}
 }
